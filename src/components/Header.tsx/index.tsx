231b41f02556986d82f89275d2b7b58a9b88cd66
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-11-24 09:21:26 -0300

    updated header backgound color

--- src/components/Header.tsx/index.tsx
+++ src/components/Header.tsx/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header(){
 
   return (
-    <Flex background="red" justify="center" padding="6">
+    <Flex justify="center" padding="6">
       <Image src="/Logo.png" alt="" width={180} height={45}/>
     </Flex>
   );
